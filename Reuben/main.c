8748c52a8b5a61aaf0411eefa63e46f0c7902475
ReubenNeate <81694170+ReubenNeate@users.noreply.github.com>
2021-04-03 19:25:54 +0200

    Update main.c

--- Reuben/main.c
+++ Reuben/main.c
@@ -2,13 +2,19 @@
 #include "stm32f4xx.h"
 #include "stm32f429i_discovery.h"
 #include "stm32f429i_discovery_lcd.h"
-#include "stdlib.h"
-#include "math.h"
-#include "stdio.h"
+
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <complex.h>
+#include <string.h>
 
 ///////////////////DECLARATIONS////////////////////////////////////////////////
 
-int N = 256; // The order of the FFT
+int N = 16; // The order of the FFT
+
+#define M_PI acos(-1.0)
+#define I sqrt(-1)
 
 void SystemClock_Config(void);
 void LCD_Intitialisation(void);
@@ -17,25 +23,97 @@ void PlotFunction(double F[N]);
 
 
 
+
+
 ///////////////////////////////////////////////////////////////////////////////
 
 int main(void){
-	//SO we start by initialsing
-HAL_Init();
-	//Intialise the system clock next
-SystemClock_Config();
-	//Now initialise the LCD and setup it's parameters and conditions
-LCD_Intitialisation();
-LCD_SetupAxes();
+		//SO we start by initialsing
+	HAL_Init();
+		//Intialise the system clock next
+	SystemClock_Config();
+		//Now initialise the LCD and setup it's parameters and conditions
+	LCD_Intitialisation();
+	LCD_SetupAxes();
 
-//Let's define a testing input signal
-double Y [N];
-double t = 0.000025;//1/5000; //Minimum timestep
-int f = 500; //Define the signal frequency
-//Now assign it to a variable
-for (int i = 0; i<N ; i = i+1){
-	Y[i] = 2000*sin(2*3.1415167*(f*t)*i)+2000;
-}
+	//Let's define a testing input signal
+	double Y [N];
+	double t = 0.000025;//1/5000; //Minimum timestep
+	int f = 500; //Define the signal frequency
+	//Now assign it to a variable
+	for (int i = 0; i<N ; i++){
+		Y[i] = 2000*sin(2*M_PI*(f*t)*i)+2000;
+		}
+
+
+
+	//Now lets Define the Twiddle Factors
+	float twiddle_real[N];			/*Create an empty complex array*/
+	float twiddle_imag[N];
+	for (int i = 0; i< N; i++){
+		twiddle_real[i] = 0;
+		twiddle_imag[i] = 0;
+	}
+	for (int i=0;i< 2*N;i++){
+		float complex twiddle = expf(-((I*2*M_PI*i)/N));	/*Twiddle factor formula*/
+		twiddle_real[i] = creal(twiddle);
+		twiddle_imag[i] = cimag(twiddle);
+	}
+
+
+	//Now let's sort the data for butterfly processing
+
+	/////////YEAH this will come later
+
+
+	//Now let's do the Butterfly processing
+	//start by declaring the variables and arrays that will be needed
+	float YSorted [16] = {1.0, 1.0000000000000013, 1.0000000000000007, 1.0000000000000029, 0.9999999999999997, 1.0000000000000007, 0.9999999999999986, 1.0000000000000009, 2.0, 2.0, 2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.7071067811865475, 1.707106781186548, 1.707106781186548, 1.7071067811865475, 0.29289321881345254, 0.29289321881344976, 0.2928932188134521, 0.2928932188134524, 1.7071067811865472, 1.7071067811865475, 1.707106781186547, 1.7071067811865461, 0.2928932188134523, 0.2928932188134513, 0.29289321881345276, 0.29289321881345365};
+	float YT_real [N];
+	float YT_imag [N];
+
+	int twdAreal;
+	int twdAimag;
+	int twdBreal;
+	int twdBimag;
+
+	for (int i = 0; i< N; i++){		/*Make YT a empty complex array*/
+		YT_real[i] = 0;
+		YT_imag[i] = 0;
+	}
+	/*First we need to loop for each layer of the butterfly diagram*/
+	for (int i=0; i<log2(N); i++){
+		int s = pow(-2,i);					/*the skipping factor*/
+		for (int j=0; j<N; j++){
+			if (j%((int)pow(2,i))){	/*Make the  skipping factor + or -*/
+				s = s * -1;
+			}
+			/*Now we need to get the correct twiddle factors*/
+			if (s>0){						/*If we retrieving from bellow*/
+				twdAreal = 1;
+				twdAimag = 0;
+				twdBreal = twiddle_real[((int)(j*(N/(pow(2,i+1))))%N)];
+				twdBimag = twiddle_imag[((int)(j*(N/(pow(2,i+1))))%N)];
+			}
+			else{							/*else retrieve from above*/
+				twdBreal = 1;
+				twdBimag = 0;
+				twdAreal = twiddle_real[((int)(j*(N/(pow(2,i+1))))%N)];
+				twdAimag = twiddle_imag[((int)(j*(N/(pow(2,i+1))))%N)];
+			}
+			YT_real[j] = (twdAreal*YSorted[j])+(twdBreal*YSorted[j+s]);
+			YT_imag[j] = (twdAimag*YSorted[j])+(twdBimag*YSorted[j+s]);
+		}
+		for (int i=0; i<N; i++){
+			YSorted[i] = YT_real[i];
+		}
+	}
+	for (int j=0; j<N; j++){
+		Y[j] = YSorted[j];
+	}
+
+
+
 
 //Now we will enter into the main loop of the program
 while(1){
@@ -48,36 +126,36 @@ while(1){
 //The system clock initialisation process
 //It's a bit long and there is a lot going on there
 void SystemClock_Config(void){
-RCC_ClkInitTypeDef RCC_ClkInitStruct;
-RCC_OscInitTypeDef RCC_OscInitStruct;
-// Enable Power Control clock
-__PWR_CLK_ENABLE();
-// The voltage scaling allows optimizing the power consumption when the
-// device is clocked below the maximum system frequency, to update the
-// voltage scaling value regarding system frequency refer to product
-// datasheet.
-__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-// Enable HSE Oscillator and activate PLL with HSE as source
-RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-// This assumes the HSE_VALUE is a multiple of 1MHz. If this is not
-// your case, you have to recompute these PLL constants.
-RCC_OscInitStruct.PLL.PLLM = (HSE_VALUE/1000000u);
-RCC_OscInitStruct.PLL.PLLN = 336;
-RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-RCC_OscInitStruct.PLL.PLLQ = 7;
-HAL_RCC_OscConfig(&RCC_OscInitStruct);
-// Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
-// clocks dividers
-RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK
-| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	// Enable Power Control clock
+	__PWR_CLK_ENABLE();
+	// The voltage scaling allows optimizing the power consumption when the
+	// device is clocked below the maximum system frequency, to update the
+	// voltage scaling value regarding system frequency refer to product
+	// datasheet.
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	// Enable HSE Oscillator and activate PLL with HSE as source
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	// This assumes the HSE_VALUE is a multiple of 1MHz. If this is not
+	// your case, you have to recompute these PLL constants.
+	RCC_OscInitStruct.PLL.PLLM = (HSE_VALUE/1000000u);
+	RCC_OscInitStruct.PLL.PLLN = 336;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 7;
+	HAL_RCC_OscConfig(&RCC_OscInitStruct);
+	// Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+	// clocks dividers
+	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK
+	| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
 }
 
 ///////////LCD_Initialise
@@ -113,21 +191,25 @@ void LCD_SetupAxes(void)
 void PlotFunction(double F[N]){
 	BSP_LCD_SetTextColor(LCD_COLOR_RED);
 //Scale F to the desired values
-	for (int i = 0; i<(N/2); i = i+1){
-		F[i] =  F[i]/22;
+	float FO [N];
+	for (int i = 0; i<(N/2); i++){
+		FO[i] =  (20*F[i]);
 	}
 //Now plot the data
 	int j = 41;
-	for (int i = 0; i <(N/2); i = i+1){
+	for (int i = 0; i < (N/2); i++){
 		//We are making every 2 pixels 1 data bin.
-		BSP_LCD_DrawVLine(j, 31,(int) F[i]);
-		j = j+1;
-		BSP_LCD_DrawVLine(j, 31, (int) F[i]);
-		j=j+1;
+		//Add a loop here to make sure it uses up the full available space
+		while (j <= 2*((i+1)*(256/N))){
+			BSP_LCD_DrawVLine(j, 31, (int) FO[i]);
+			j = j+1;
+		}
 	}
 
 }
-
+//////////OddEven
+//This function will divide an array by it's index.
+//Odd indexes first, even indexes second
 
 /////////////////////////////////////END///////////////////////////////////////
 
