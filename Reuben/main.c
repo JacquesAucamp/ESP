fd0a810c464f969d4f902ad6b6ff6bfc932e9e97
ReubenNeate <81694170+ReubenNeate@users.noreply.github.com>
2021-04-04 23:09:03 +0200

    Update main.c

--- Reuben/main.c
+++ Reuben/main.c
@@ -8,18 +8,36 @@
 #include <stdio.h>
 #include <complex.h>
 #include <string.h>
+#include <stdbool.h>
 
 ///////////////////DECLARATIONS////////////////////////////////////////////////
 
+//Private Variables ----------------------------------------------------------
 int N = 256; // The order of the FFT
+float twiddle_real[256];			/*Create an empty complex array*/
+float twiddle_imag[256];		//Used to store the twiddle factors
+float ADC_1 [256];
+float ADC_2 [256];
+bool flag_1 = 0;
+bool flag_2 = 0;
+int ADCCounter = 0;
 
 #define M_PI acos(-1.0)
 #define I sqrt(-1)
 
+//----------------------------------------------------------------------------
+
+//Private Functions-----------------------------------------------------------
 void SystemClock_Config(void);
 void LCD_Intitialisation(void);
 void LCD_SetupAxes(void);
 void PlotFunction(float F[N]);
+void ProcessData(float Y[N]);
+void TwiddleInit(void);
+
+
+
+
 
 
 
@@ -35,143 +53,64 @@ int main(void){
 		//Now initialise the LCD and setup it's parameters and conditions
 	LCD_Intitialisation();
 	LCD_SetupAxes();
+		//Now lets Define the Twiddle Factors
+	TwiddleInit();
 
-	//Let's define a testing input signal
-	float Y [N];
-	float t = 0.000025;//1/5000; //Minimum timestep
-	int f = 500; //Define the signal frequency
-	//Now assign it to a variable
-	for (int i = 0; i<N ; i++){
-		Y[i] = 2000*sin(2*M_PI*(f*t)*i)+2000;
-		//Y[i] = 10*i;
+
+	//Place the bellow code into the subroutine that the ADC interrupt calls
+	/*if (ADCCounter < N){
+		ADC_1[ADCCounter] = adc_value;
+		flag_1 = 1;
+		ADCCounter ++;
 	}
-
-	float YOG [N];
-	for (int i = 0; i<N; i++){
-		YOG[i] = Y[i];
+	else {
+		ADC_1[ADCCounter - N] = adc_value;
+		flag_2 = 1;
+		ADCCounter ++;
 	}
+	if (ADCCounter == 2*N){
+		ADCCounter = 0;
+	}*/
 
 
 
-	//Now lets Define the Twiddle Factors
-	float twiddle_real[N];			/*Create an empty complex array*/
-	float twiddle_imag[N];
-	for (int i = 0; i< N; i++){
-		twiddle_real[i] = 0;
-		twiddle_imag[i] = 0;
-	}
-	//float complex twiddle;
-	for (int i=0; i< N; i++){
-	//	twiddle = expf(-((I*2*M_PI*i)/N));	/*Twiddle factor formula*/
-	//	twiddle_real[i] = creal(twiddle);
-	//	twiddle_imag[i] = cimag(twiddle);
-		twiddle_real[i] = cos((2*M_PI*i)/N);
-		twiddle_imag[i] = sin((2*M_PI*i)/N);
-	}
-
-
-
-	//PlotFunction(Y);
-
-	//Now let's sort the data for butterfly processing
-	float Ytemp [N];
-
-	for (int i = 0; i < (log2(N)-1); i++){
-		int p = pow(2,i);
-		float oddeven_arr[N/p];
-				//oddeven function now
-		for (int j = 0; j < N; j = j + (N/p)){
-			for (int k = j; k < j+(N/p); k++){//Divide up the data
-				oddeven_arr[k] = Y[k];
-
-			}
-
-			int L = N/p;//sizeof(oddeven_arr);
-			float z[L];						/*empty array*/
-			for (int k = 0; k < L/2; k++){			/*Only need to loop through half the*/
-				z[k] = oddeven_arr[2*k];			/*length. Will place every even sample at the sfirst*/
-				//z[L-k-1] = oddeven_arr[L-(2*k)-1];	/*half and every odd sample at the second half*/
-			}
-			for (int k =0; k < L/2; k++){
-				z[L-k-1] = oddeven_arr[L-(2*k)-1];
-			}
-
-			for (int k = j; k < j + (N/p); k++){//Return the ordered sata
-				Ytemp[k] = z[k];
-			}
-		}
-		for (int k = 0; k < N; k++){
-			Y[k] = Ytemp[k];
-		}
-	}
-
-
-	//PlotFunction(Y);
-
-
-
-
-
-
-	//Now let's do the Butterfly processing
-	//start by declaring the variables and arrays that will be needed
-	//float YSorted [16] = {1.0, 1.0000000000000013, 1.0000000000000007, 1.0000000000000029, 0.9999999999999997, 1.0000000000000007, 0.9999999999999986, 1.0000000000000009, 2.0, 2.0, 2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.7071067811865475, 1.707106781186548, 1.707106781186548, 1.7071067811865475, 0.29289321881345254, 0.29289321881344976, 0.2928932188134521, 0.2928932188134524, 1.7071067811865472, 1.7071067811865475, 1.707106781186547, 1.7071067811865461, 0.2928932188134523, 0.2928932188134513, 0.29289321881345276, 0.29289321881345365};
-	float YSorted [sizeof(Y)/sizeof(float)];
-	for (int k = 0; k < sizeof(Y)/sizeof(float); k++){
-		YSorted[k] = Y[k];
-	}
-	float YT_real [N];
-	float YT_imag [N];
-	int twdAreal;
-	int twdAimag;
-	int twdBreal;
-	int twdBimag;
-
-	for (int i = 0; i< N; i++){		/*Make YT a empty complex array*/
-		YT_real[i] = 0;
-		YT_imag[i] = 0;
-	}
-		/*First we need to loop for each layer of the butterfly diagram*/
-	for (int i=0; i<log2(N); i++){
-		int s = -pow(2,i);					/*the skipping factor*/
-		for (int j=0; j<N; j++){
-			if (j % ((int)(pow(2,i))) == 0){	/*Make the  skipping factor + or -*/
-				s = s * -1;
-			}
-				/*Now we need to get the correct twiddle factors*/
-			if (s > 0){						/*If we retrieving from bellow*/
-				twdAreal = 1;
-				twdAimag = 1;
-				twdBreal = twiddle_real[((int)(j*(N/(pow(2,i+1))))%N)];
-				twdBimag = twiddle_imag[((int)(j*(N/(pow(2,i+1))))%N)];
-			}
-			else{							/*else retrieve from above*/
-				twdBreal = 1;
-				twdBimag = 1;
-				twdAreal = twiddle_real[((int)(j*(N/(pow(2,i+1))))%N)];
-				twdAimag = twiddle_imag[((int)(j*(N/(pow(2,i+1))))%N)];
-			}
-			YT_real[j] = (twdAreal*YSorted[j])+(twdBreal*YSorted[j+s]);
-			YT_imag[j] = (twdAimag*YSorted[j])+(twdBimag*YSorted[j+s]);
-		}
-		for (int i=0; i<N; i++){
-			YSorted[i] = YT_real[i];
-		}
-	}
-	for (int j=0; j<N; j++){
-		Y[j] = YSorted[j];
-	}
-
-
-	PlotFunction(Y);
 
 
 //Now we will enter into the main loop of the program
 while(1){
+	//Let's define a testing input signal
+		/*float Y [N];
+		float t = 0.000025;//1/4000; //Minimum timestep
+		int f = 10000; //Define the signal frequency
+		//Now assign it to a variable
+		for (int i = 0; i<N ; i++){
+			Y[i] = 2000*sin(2*M_PI*(f*t)*i)+2000;
+			//Y[i] = 10*i;
+		}
+	flag_1 = 1;
+	for (int i = 0; i < N; i++){
+		ADC_1[i] = Y[i];
+	}*/
+
+
+	if (flag_1 == 1){
+		ProcessData(ADC_1);
+		flag_1 = 0;
+	}
+	else if (flag_2 == 1){
+		ProcessData(ADC_2);
+		flag_2 = 0;
+	}
 
 
 	}
 }
+
+
+
+
+
+
 
 /////////////////////////SUBROUTINES///////////////////////////////////////////
 /////////////////////CLOCK
@@ -226,6 +165,7 @@ void LCD_Intitialisation(void)
 }
 /////////LCD AXES setup
 void LCD_SetupAxes(void)
+
 {
 	BSP_LCD_Clear(LCD_COLOR_BLACK);  //Clear and set the colour
 	BSP_LCD_SetTextColor(LCD_COLOR_GREEN); //Set the axis colour
@@ -239,23 +179,45 @@ void LCD_SetupAxes(void)
 	BSP_LCD_DisplayChar(165,0,0x48);
 	BSP_LCD_DisplayChar(180,0,0x7A);
 }
+//////////Calculate the Twiddle factors
+void TwiddleInit(void){
+	for (int i = 0; i< N; i++){
+		twiddle_real[i] = 0;
+		twiddle_imag[i] = 0;
+	}
+	//float complex twiddle;
+	for (int i=0; i< N; i++){
+	//	twiddle = expf(-((I*2*M_PI*i)/N));	/*Twiddle factor formula*/
+	//	twiddle_real[i] = creal(twiddle);
+	//	twiddle_imag[i] = cimag(twiddle);
+		twiddle_real[i] = cos((2*M_PI*i)/N);
+		twiddle_imag[i] = sin((2*M_PI*i)/N);
+	}
+}
 //////////Plot a given function
 void PlotFunction(float F[N]){
-	BSP_LCD_SetTextColor(LCD_COLOR_RED);
+
 //Scale F to the desired values
 	float FO [N];
 	for (int i = 0; i < (N); i++){
-		FO[i] =  (F[i]/(22*N));
+		FO[i] =  2*20*log10(F[i]/(N));
+		//FO[i] =  (F[i]/(22));
 	}
+//Let's clear the display
+	BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
+	BSP_LCD_FillRect(41,31,260,200);
+	BSP_LCD_SetTextColor(LCD_COLOR_RED);
 //Now plot the data
 	int j = 41;
-	for (int i = 0; i < (N); i++){
+	for (int i = 0; i < (N/2); i++){
 		//We are making every 2 pixels 1 data bin.
 		//Add a loop here to make sure it uses up the full available space
-		BSP_LCD_DrawVLine(j, 31,  FO[i]);
-		j = j+1;
-		BSP_LCD_DrawVLine(j, 31,  FO[i]);
-		j = j+1;
+		while (j < (i+1)*(256/(N/2)) + 41){
+			BSP_LCD_DrawVLine(j, 31,  FO[i]);
+			j = j+1;
+		}
+
+
 
 
 		//BSP_LCD_DrawVLine(j, 31,  FO[i]);
@@ -264,5 +226,94 @@ void PlotFunction(float F[N]){
 	}
 
 }
+///////////Process the data with an FFT algortihm
+void ProcessData(float Y[N]){
+
+
+	float Ytemp [N]; //Create a temporary or working array
+		for (int i = 0; i < (log2(N))-1; i++){
+			int p = pow(2,i);
+			float oddeven_arr[N/p];
+					//oddeven function now
+			for (int j = 0; j < N; j = j + (N/p)){
+				int n = 0;
+				for (int k = j; k < j+(N/p); k++){//Divide up the data
+					oddeven_arr[n] = Y[k];
+					n++;
+				}
+				int L = N/p;//sizeof(oddeven_arr);
+				float z[L];						/*empty array*/
+				for (int k = 0; k < L/2; k++){			/*Only need to loop through half the*/
+					z[k] = oddeven_arr[2*k];			/*length. Will place every even sample at the sfirst*/
+					z[L-k-1] = oddeven_arr[L-(2*k)-1];	/*half and every odd sample at the second half*/
+				}
+				n = 0;
+				for (int k = j; k < j + (N/p); k++){//Return the ordered sata
+					Ytemp[k] = z[n];
+					n++;
+				}
+			}
+			for (int k = 0; k < N; k++){
+				Y[k] = Ytemp[k];
+			}
+		}
+		//PlotFunction(Y);
+
+		//Now let's do the Butterfly processing
+		//start by declaring the variables and arrays that will be needed
+		float YSorted [N];
+		for (int k = 0; k < N; k++){
+			YSorted[k] = Y[k];
+		}
+		float YT_real [N];
+		float YT_imag [N];
+		int twdAreal;
+		int twdAimag;
+		int twdBreal;
+		int twdBimag;
+
+		for (int i = 0; i< N; i++){		/*Make YT a empty complex array*/
+			YT_real[i] = 0;
+			YT_imag[i] = 0;
+		}
+			/*First we need to loop for each layer of the butterfly diagram*/
+		for (int i=0; i<log2(N); i++){
+			int s = -pow(2,i);					/*the skipping factor*/
+			for (int j=0; j<N; j++){
+				if (j % ((int)(pow(2,i))) == 0){	/*Make the  skipping factor + or -*/
+					s = s * -1;
+				}
+					/*Now we need to get the correct twiddle factors*/
+				if (s > 0){						/*If we retrieving from bellow*/
+					twdAreal = 1;
+					twdAimag = 0;
+					twdBreal = twiddle_real[((int)(j*(N/(pow(2,i+1))))%N)];
+					twdBimag = twiddle_imag[((int)(j*(N/(pow(2,i+1))))%N)];
+				}
+				else{							/*else retrieve from above*/
+					twdBreal = 1;
+					twdBimag = 0;
+					twdAreal = twiddle_real[((int)(j*(N/(pow(2,i+1))))%N)];
+					twdAimag = twiddle_imag[((int)(j*(N/(pow(2,i+1))))%N)];
+				}
+				YT_real[j] = (twdAreal*YSorted[j])+(twdBreal*YSorted[j+s]);
+				YT_imag[j] = (twdAimag*YSorted[j])+(twdBimag*YSorted[j+s]);
+			}
+			for (int i=0; i<N; i++){
+				YSorted[i] = YT_real[i]+YT_imag[i];
+			}
+		}
+		for (int j=0; j<N; j++){
+			Y[j] = YSorted[j];
+		}
+
+		PlotFunction(Y);
+
+
+}
+
+
+
+
 
 /////////////////////////////////////END///////////////////////////////////////
